e70a4a1bc0f37d82c93cec915aeee27d322ec8bd
Utkarsh <srivastava.utk@gmail.com>
2021-04-25 17:45:32 +0530

    Added profiles viewset and permissions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,10 @@ from profiles_api import views
 router = DefaultRouter()
 # the router will already create 4 routes for us so we don't need to define a forward slash here
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+# no need to provide a basename because we have a query set. If we have a queryset
+# Django will figure out the name from the model thats assigned to it
+router.register('profile', views.UserProfileViewSet)
+
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
